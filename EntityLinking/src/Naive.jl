01de5ca59fedca39384f6d8d0133a7b59bdb6b5a
Rolf Jagerman <rjagerman@gmail.com>
2015-03-05 11:14:27 +0100

    Removed empty functions from naive entity linking

--- EntityLinking/src/Naive.jl
+++ EntityLinking/src/Naive.jl
@@ -2,11 +2,3 @@
 #
 # Naive
 #   Contains the naive entity linking algorithm
-
-function naive_linking(tokens, inv_entities, scores, sessions)
-
-end
-
-function naive_link(query, tokens, inv_entities, scores)
-
-end
