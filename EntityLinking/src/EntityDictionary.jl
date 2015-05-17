3c37c9f035bc9831f3478d67bd39d264d2aa7281
Rolf Jagerman <rjagerman@gmail.com>
2015-05-17 13:55:02 +0200

    Made dictionary lowercase with sorting on prior

--- EntityLinking/src/EntityDictionary.jl
+++ EntityLinking/src/EntityDictionary.jl
@@ -7,6 +7,7 @@ type Entity
     Entity(uri::String) = new(uri, 0.0)
     Entity(uri::String, prior::Float64) = new(uri, prior)
 end
+Base.isless(e1::Entity, e2::Entity) = e1.prior < e2.prior
 
 # An entity dictionary, mapping tokens to an array of entities
 typealias EntityDictionary Dict{String, Array{Entity}}
@@ -16,7 +17,11 @@ function read_crosswikis(crosswiki::String)
     dictionary = EntityDictionary()
     for line in filter(line -> !isempty(line), eachline(gzopen(crosswiki)))
         token, (uri, score) = (split(line, "\t")[1], split(split(line, "\t")[2]))
+        token = lowercase(token)
         push!(get!(dictionary, token, Entity[]), Entity(uri, float64(score)))
+    end
+    for (k,v) in dictionary
+        sort!(v, rev=true)
     end
     dictionary
 end
